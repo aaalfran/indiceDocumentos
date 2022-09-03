b205b1dba9df08fab4224ba465b71d4eeac701ff
aaalfran <aaalfran@espol.edu.ec>
2022-09-02 22:49:02 -0500

    Cambios para obtener linea

--- main.py
+++ main.py
@@ -2,37 +2,28 @@ import os
 def createDictionary():
     palabras = {}
     cwd = os.getcwd()
-    os.chdir(os.getcwd()+'\\documentos')
+    os.chdir(os.getcwd()+'/documentos')
     fileList = os.listdir(os.getcwd())
     for file in fileList:
 
         with open(file, 'r') as f:
 
-            words = f.read().lower().split()
-            for word in words:
+            lines = f.readlines()
+            lineNumber = 1
+            for line in lines:
+                dic = {}
+                words = line.lower().split()
+                for word in words:
+                    if word[-1] in [',', '!', '?', '.']:
+                        word = word[:-1]
+                    if word not in palabras.keys():
+                        dic[f.name] = [lineNumber]
+                        palabras[word] = dic
+                    elif file not in palabras[word]:
+                        palabras[word][f.name] = [lineNumber]
+                    elif lineNumber not in palabras[word][f.name]:
+                        palabras[word][f.name] += [lineNumber]
 
-                if word[-1] in [',', '!', '?', '.',"'"]:
-                    word = word[:-1]
-                if word not in palabras.keys():
-                    palabras[word] = [f.name]
-
-                else:
-                    if file not in palabras[word]:
-                        palabras[word] += [f.name]
-
+                lineNumber += 1
     return palabras, cwd
-
-
-def writeToFile(words, cwd):
-    os.chdir(cwd)
-    with open('indice.txt', 'w') as indexFile:
-
-        for word, files in words.items():
-            indexFile.write(word + " ")
-            for file in files:
-                indexFile.write(file[:file.find(".txt")] + " ")
-
-            indexFile.write(f'{len(files)}\n')
-
-
-writeToFile(*createDictionary())
+createDictionary()
